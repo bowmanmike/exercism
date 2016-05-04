19cb0a01075605e877096178cf55fc5db001e1e7
Mike Bowman <mbowman2@gmail.com>
2016-05-04 12:46:34 -0400

    remove commented code

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -54,26 +54,3 @@ class Fixnum
 
   VERSION = 1
 end
-
-# value = self
-# result = []
-#
-#
-#
-# while value < 10 && value >= 5
-#   result << "V"
-#   value -= 5
-# end
-#
-# if value < 5
-#   value.times do
-#     result << "I"
-#     value -= 1
-#   end
-# end
-#
-# result.join.gsub(/IIII/, "IV")
-
-# def frequency(ary)
-#   ary.each_with_object(Hash.new(0)) { |key, hash| hash[key] += 1}
-# end
