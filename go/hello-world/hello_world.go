a954199c58fa5926bd7c28be7aff4039344efd5c
Mike Bowman <mbowman2@gmail.com>
2017-01-05 14:26:21 -0500

    Update go thingy

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -6,13 +6,13 @@ const testVersion = 2
 
 // HelloWorld greets the user to name, if provided
 func HelloWorld(str string) string {
-    var name string
+	var name string
 
-    if str == "" {
-        name = "World"
-    } else {
-        name = str
-    }
-    var greeting = fmt.Sprintf("Hello, %s!", name)
-    return greeting
+	if str == "" {
+		name = "World"
+	} else {
+		name = str
+	}
+	var greeting = fmt.Sprintf("Hello, %s!", name)
+	return greeting
 }
