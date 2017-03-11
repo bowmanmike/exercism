6351374eac48524e0571aa40d6844e7c030d68ef
Mike Bowman <mbowman2@gmail.com>
2017-03-11 12:21:19 -0500

    Complete acronym.go

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,16 +1,34 @@
 package acronym
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+	"unicode"
+)
 
 const testVersion = 2
 
 func Abbreviate(s string) string {
 	var result []string
-	spl := strings.Split(s, " ")
 
-	for _, word := range spl {
-		result = append(result, string(word[0]))
+	r := regexp.MustCompile("[^A-Za-z0-9]")
+	ary := r.Split(s, -1)
+
+	for _, word := range ary {
+		for i, char := range word {
+			firstChar := i == 0
+			if firstChar {
+				result = append(result, string(char))
+			} else if i < len(word)-1 {
+				previousChar := word[i-1]
+				nextChar := word[i+1]
+				lowerSurround := unicode.IsLower(rune(previousChar)) && unicode.IsLower(rune(nextChar))
+				if unicode.IsUpper(char) && lowerSurround {
+					result = append(result, string(char))
+				}
+			}
+		}
 	}
 
-	return strings.Join(result, "")
+	return strings.ToUpper(strings.Join(result, ""))
 }
