9a9cf605461454a7085d24b3cfaedbeddd57547b
Mike Bowman <mbowman2@gmail.com>
2017-03-13 09:49:29 -0400

    Update go hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -8,11 +8,11 @@ func Distance(a, b string) (int, error) {
 	aBytes := []byte(a)
 	bBytes := []byte(b)
 	distance := 0
+	if len(aBytes) != len(bBytes) {
+		return distance, errors.New("different length strands")
+	}
 
 	for i, char := range aBytes {
-		if len(aBytes) != len(bBytes) {
-			return distance, errors.New("different length strands")
-		}
 		if bBytes[i] != char {
 			distance++
 		}
