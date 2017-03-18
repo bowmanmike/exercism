39b2faed2def274cb491bb3c4061243def20a0a7
Mike Bowman <mbowman2@gmail.com>
2017-03-18 15:06:17 -0400

    Start triangle

---
+++ go/triangle/triangle.go
@@ -0,0 +1,21 @@
+package triangle
+
+const testVersion = 3
+
+// Code this function.
+func KindFromSides(a, b, c float64) Kind {
+        Kind{"Equ"}
+}
+
+// Notice it returns this type.  Pick something suitable.
+type Kind struct {
+        Name string
+}
+
+// Pick values for the following identifiers used by the test program.
+NaT // not a triangle
+Equ // equilateral
+Iso // isosceles
+Sca // scalene
+
+// Organize your code for readability.
