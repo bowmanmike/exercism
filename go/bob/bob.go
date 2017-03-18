ef3e730620518c423a525342a9bd71768c27ee81
Mike Bowman <mbowman2@gmail.com>
2017-03-18 14:49:42 -0400

    Finish bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,5 +1,9 @@
 package bob // package name must match the package name in bob_test.go
-import "unicode"
+import (
+	"regexp"
+	"strings"
+	"unicode"
+)
 
 const testVersion = 2 // same as targetTestVersion
 
@@ -9,20 +13,36 @@ const nonsenseResponse = "Fine. Be that way!"
 const defaultResponse = "Whatever."
 
 func Hey(s string) string {
-	lastChar := string(s[len(s)-1])
+	validCharacters, _ := regexp.MatchString(`[A-Za-z0-9\,\?!]`, s)
+	if len(s) == 0 || !validCharacters {
+		return nonsenseResponse
+	}
+
+	s = strings.TrimSpace(s)
+
+	// lastChar := string(s[len(s)-1])
+	lastChar := "?"
 	allCaps := true
+	containsLetters := false
+	containsNumbers := false
 
 	for _, char := range s {
 		if unicode.IsLetter(char) {
+			containsLetters = true
 			if !unicode.IsUpper(char) {
 				allCaps = false
 			}
 		}
+		if unicode.IsNumber(char) {
+			containsNumbers = true
+		}
 	}
-	if allCaps {
+	if allCaps && containsLetters {
 		return shoutResponse
 	} else if lastChar == "?" {
 		return questionResponse
+	} else if !containsLetters && !containsNumbers {
+		return nonsenseResponse
 	}
 
 	return defaultResponse
