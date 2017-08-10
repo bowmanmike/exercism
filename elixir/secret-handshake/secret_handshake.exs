9a77458ec7effebfdfb8da62fbfa3f844feff188
Mike Bowman <mbowman2@gmail.com>
2017-08-10 14:58:21 -0400

    Pass single action tests

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,11 +1,11 @@
 defmodule SecretHandshake do
-  @cmd_map %{
-    1 => "wink",
-    10 => "double blink",
-    100 => "close your eyes",
-    1000 => "jump",
-    10000 => "reverse order"
-  }
+  # @cmd_map %{
+  #   1 => "wink",
+  #   10 => "double blink",
+  #   100 => "close your eyes",
+  #   1000 => "jump",
+  #   10000 => "reverse order"
+  # }
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -21,12 +21,16 @@ defmodule SecretHandshake do
   10000 = Reverse the order of the operations in the secret handshake
   """
   @spec commands(code :: integer) :: list(String.t())
-  def commands(code), do: _commands(Integer.to_charlist code, 2)
+  def commands(code) do
+    n = Integer.to_charlist code, 2
+    IO.puts "CALLING WITH: #{n}"
+    _commands([n])
+  end
 
   defp _commands([]), do: []
-  defp _commands([ 0 | tail ]),    do: [_commands(tail)]
-  defp _commands([ 1000 | tail ]), do: ["jump" | _commands(tail)]
-  defp _commands([ 100 | tail ]),  do: ["close your eyes" | _commands(tail)]
-  defp _commands([ 10 | tail ]),   do: ["double blink" | _commands(tail)]
-  defp _commands([ 1 | tail ]),    do: ["wink" | _commands(tail)]
+  defp _commands([ '0' | tail ]),    do: [_commands(tail)]
+  defp _commands([ '1000' | tail ]), do: ["jump" | _commands(tail)]
+  defp _commands([ '100' | tail ]),  do: ["close your eyes" | _commands(tail)]
+  defp _commands([ '10' | tail ]),   do: ["double blink" | _commands(tail)]
+  defp _commands([ '1' | tail ]),    do: ["wink" | _commands(tail)]
 end
