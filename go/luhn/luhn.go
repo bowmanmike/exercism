8f08dbb29f29221c008fab6a19ce29ffc518a864
Mike Bowman <mbowman2@gmail.com>
2017-11-30 09:58:12 -0500

    Document and clean up luhn go

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,30 +1,42 @@
+// Package luhn contains functions to validate a string
+// against the Luhn algorithm.
 package luhn
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
+// Valid returns whether or not the provided string is valid
+// under the Luhn algorithm.
 func Valid(number string) bool {
+	// Delete whitespace from the provided string
 	r := regexp.MustCompile(`\s+`)
 	number = r.ReplaceAllString(number, "")
 
+	// Split the string into a slice of characteres
 	digits := strings.Split(number, "")
 	if len(digits) <= 1 {
 		return false
 	}
 
 	total := 0
+	// Loop over digits in reverse order
 	for i := len(digits) - 1; i >= 0; i-- {
 		n, err := strconv.Atoi(digits[i])
 		if err != nil {
-			fmt.Println(err)
+			// Skip entry if the character cannot be converted
+			// to an int
 			continue
 		}
+		// This section is confusing
+		// Starting with the last element, perform checks on every
+		// second element
 		if i%2 != 0 || i == 0 {
+			// Double element
 			n = n * 2
+			// If greater than 9, subtract 9
 			if n > 9 {
 				n = n - 9
 			}
