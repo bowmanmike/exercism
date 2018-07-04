eaea79d636123b5cbdc322839b45181efab38fee
Mike Bowman <mbowman2@gmail.com>
2018-07-04 18:14:57 -0400

    Working on rotational cipher

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -7,6 +7,26 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) when rem(shift, 26) == 0, do: text
+
   def rotate(text, shift) do
+    parsed_shift = rem(shift, 26)
+    _rotate(text, parsed_shift)
+  end
+
+  def _rotate(text, shift) do
+    text
+    |> String.split()
+    # |> Enum.reject(&(&1 == "" || &1 == " "))
+    |> Enum.map(fn n ->
+      <<x::utf8>> = n
+
+      if String.match?(n, ~r/[a-z|A-Z]/) do
+        x + shift
+      else
+        x
+      end
+    end)
+    |> List.to_string()
   end
 end
