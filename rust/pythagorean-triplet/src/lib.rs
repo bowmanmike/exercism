c0a185da3cee927e6a070ca604bed2949b238354
Mike Bowman <mbowman2@gmail.com>
2018-08-08 21:46:11 -0400

    Start rust track

---
+++ rust/pythagorean-triplet/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn find() -> Option<u32> {
+    // Find triplet (a**2 + b**2 == c**2) where a + b + c == 1000
+    // Should return 31875000
+    let max: u32 = 1000;
+    let a: u32;
+    let b: u32;
+    let c: u32;
+
+    for i in 1..max {
+        for j in 1..max {
+            for k in 1..max {
+                if j.pow(2) + k.pow(2) == i.pow(2) {
+                    if i + j + k == 1000 {
+                        a = i;
+                        b = j;
+                        c = k;
+                    }
+                }
+            }
+        }
+    }
+    let result: u32 = a * b * c;
+    // Some(result);
+    None;
+}
