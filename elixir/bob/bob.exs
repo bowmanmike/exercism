482c1eb943c2e8dd90a6d058bdd91b472ba668dc
Mike Bowman <mbowman2@gmail.com>
2018-08-09 22:32:34 -0400

    Refactor to helper functions

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,17 +1,28 @@
 defmodule Bob do
   def hey(phrase) do
-    is_empty? = String.trim(phrase) == ""
-    is_uppercase? = String.upcase(phrase) == phrase
-    is_question? = phrase =~ ~r(.*\?$)
-    only_numbers? = phrase =~ ~r(^[0-9\s\W]*$)
-
     cond do
-      is_empty? -> "Fine. Be that way!"
-      only_numbers? -> "Sure."
-      is_uppercase? && is_question? -> "Calm down, I know what I'm doing!"
-      is_uppercase? -> "Whoa, chill out!"
-      is_question? -> "Sure."
+      is_empty?(phrase) -> "Fine. Be that way!"
+      only_numbers?(phrase) -> "Sure."
+      is_uppercase?(phrase) && is_question?(phrase) -> "Calm down, I know what I'm doing!"
+      is_uppercase?(phrase) -> "Whoa, chill out!"
+      is_question?(phrase) -> "Sure."
       true -> "Whatever."
     end
+  end
+
+  defp is_empty?(phrase) do
+    String.trim(phrase) == ""
+  end
+
+  defp is_uppercase?(phrase) do
+    String.upcase(phrase) == phrase
+  end
+
+  defp is_question?(phrase) do
+    phrase =~ ~r(.*\?$)
+  end
+
+  defp only_numbers?(phrase) do
+    phrase =~ ~r(^[0-9\s\W]*$)
   end
 end
