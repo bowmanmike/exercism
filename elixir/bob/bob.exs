ffb08f00599a32d3649d99f8c375b2b938c9e6e6
Mike Bowman <mbowman2@gmail.com>
2018-08-09 22:24:42 -0400

    One broken test

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,9 +1,16 @@
 defmodule Bob do
   def hey(phrase) do
+    is_empty? = String.trim(phrase) == ""
+    is_uppercase? = String.upcase(phrase) == phrase
+    is_question? = phrase =~ ~r(.*\?$)
+    only_numbers? = phrase =~ ~r(^[0-9\s\W]*$)
+
     cond do
-      String.trim(phrase) == "" -> "Fine. Be that way!"
-      String.upcase(phrase) == phrase -> "Whoa, chill out!"
-      phrase =~ ~r(.*\?$) -> "Sure."
+      is_empty? -> "Fine. Be that way!"
+      only_numbers? -> "Sure."
+      is_uppercase? && is_question? -> "Calm down, I know what I'm doing!"
+      is_uppercase? -> "Whoa, chill out!"
+      is_question? -> "Sure."
       true -> "Whatever."
     end
   end
--- elixir/bob/bob_test.exs
+++ elixir/bob/bob_test.exs
@@ -67,12 +67,12 @@ defmodule BobTest do
     assert Bob.hey("  ") == "Fine. Be that way!"
   end
 
-  @tag :pending
+  # @tag :pending
   test "only numbers" do
     assert Bob.hey("1, 2, 3") == "Whatever."
   end
 
-  @tag :pending
+  # @tag :pending
   test "question with numbers" do
     assert Bob.hey("4?") == "Sure."
   end
