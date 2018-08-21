ba9c962b341810f2f6104c1fcf00a2250c3fc74d
Mike Bowman <mbowman2@gmail.com>
2018-08-21 13:43:47 -0400

    Solve hamming

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -1,5 +1,12 @@
 class Hamming
   def self.compute(first, second)
+    first_size = first.size
+
+    raise ArgumentError.new if first_size != second.size
     return 0 if first == second
+
+    (0..first_size - 1).reduce(0) do |val, n|
+      first[n] == second[n] ? val : val + 1
+    end
   end
 end
